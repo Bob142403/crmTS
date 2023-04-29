8190ac9d2d46185e12c85e11e360a4d905af15af
Bob142403 <boboshka2006@mail.ru>
2023-04-29 15:44:39 +0500

    END

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,7 +11,10 @@ export interface State {
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
-  modules: { clientsModule, usersModule },
+  modules: {
+    usersModule,
+    clientsModule,
+  },
 });
 
 export function useStore() {
